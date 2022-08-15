a65f3fd48ade9c393ffdf94312e92643ab68be8c
sherbinin9292 <sherbinin9292@mail.ru>
2022-08-15 19:43:42 +0300

    draft-second

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -17,7 +17,7 @@ export const Counter = () => {
     return (
         <Stack direction="column" spacing={5} sx={{ p: 10 }} justifyContent="center" alignItems="center" border="3px solid black">
             <Avatar alt="Vadim Sherbinin" src="https://sun1-90.userapi.com/impg/ytt4cALte_WPHlPdLLFR5nyaMuXbsRA_YgKQlQ/U8kHN8Ca99w.jpg?size=1080x1080&quality=96&sign=5cc5909fe2ebc5523958246943b4b509&type=album" sx={{ width: 100, height: 100 }} />
-            <Typography variant="h6" font-weight="bold">Как вам этот программист?</Typography>
+            <Typography variant="h6" fontWeight="bold">Как вам этот программист?</Typography>
             <Stack direction="row" spacing={2}>
                 <Typography>{selector}</Typography>
                 <FavoriteSharpIcon fontSize="small"/>
